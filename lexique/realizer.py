6d01b8fb39f2051f860911126860966025693aa6
Krolov18 <korantin.leveque@protonmail.com>
2023-01-08 23:48:39 +0100

    WIP Refactore des realizers

--- lexique/realizer.py
+++ lexique/realizer.py
@@ -1,75 +1,149 @@
 """
     Fonctions permettant de réaliser les différentes structures définies dans structures.py
 """
-from typing import Any
 
 from frozendict import frozendict  # type: ignore
 
 from lexique.applier import format_stem, apply
-from lexique.structures import (Circumfix, Forme, Gabarit,
-                                Lexeme, Prefix, Suffix,
-                                Radical, Condition, Selection, Morpheme, Term)
+from lexique.readers import select_morphemes
+from lexique.ruler import ruler_radical
+from lexique.structures import (Circumfix, Forme, Gabarit, Lexeme, Prefix, Suffix,
+                                Radical, Condition, Selection, Phonology, type_Morpheme, Paradigm)
+from utils.abstract_factory import factory_function
 
 
-def realize(term: Term | Morpheme, **kwargs: Any) -> str | tuple[str, ...] | list[Forme]:
-    match term:
-        case Lexeme(_, pos, sigma, traduction):
-            assert traduction is not None
-            output: list[Forme] = []
-            for i_sigma, i_func in kwargs.get("paradigm", {})[pos].items():
-                if not i_sigma:
-                    output.append(i_func(term))
-                    continue
-                if (sigma.items() <= i_sigma["source"].items()) and (traduction.sigma.items() <= i_sigma["destination"].items()):  # and (term.sigma["destination"].items() <= sigma["destination"].items()):
-                    output.append(i_func(term))
-            return output
-        case Forme(_, morphemes, sigma, _) as t:
-            result = ""
-            for morpheme in morphemes:
-                assert morpheme.sigma is not None, morpheme
-                m_sigma = dict(sigma)
-                m_sigma.update(dict(morpheme.sigma))
-                t.sigma = frozendict(m_sigma)
-                result = realize(term=morpheme, accumulator=result, phonology=kwargs["phonology"])
-            return result
-        case Suffix(rule, _):
-            assert rule is not None
-            accumulator = kwargs.get("accumulator", "")
-            return f"{accumulator}{rule.group(1)}"
-        case Prefix(rule, _):
-            assert rule is not None
-            accumulator = kwargs.get("accumulator", "")
-            return f"{rule.group(1)}{accumulator}"
-        case Circumfix(rule, _):
-            assert rule is not None
-            accumulator = kwargs.get("accumulator", "")
-            return f"{rule.group(1)}{accumulator}{rule.group(2)}"
-        case Radical(_, _, stem):
-            match stem:
-                case str() as a:
-                    return a
-                case [a]:
-                    return a
-                case tuple() as s:
-                    return s
-                case _:
-                    raise TypeError()
-        case Gabarit(rule, _):
-            accumulator = kwargs.get("accumulator", "")
-            phonology = kwargs.get("phonology", "")
-            return apply(rule.string,
-                         format_stem(accumulator, phonology),
-                         phonology)
-        case Selection(rule, _):
-            assert rule is not None
-            accumulator = kwargs.get("accumulator", "")
-            return accumulator[int(rule.group(1)) - 1]
-        case Condition(_, _, cond, true, false):
-            accumulator = kwargs.get("accumulator", "")
-            try:
-                _ = realize(term=cond, accumulator=accumulator)
-            except IndexError:
-                return realize(term=false, accumulator=accumulator)
-            return realize(term=true, accumulator=accumulator)
+def realize(term, **kwargs):
+    """
+    :param term:
+    :param kwargs:
+    :return:
+    """
+    return factory_function(concrete_product=f"realize_{term.__class__.__name__.lower()}",
+                            package=__name__,
+                            **kwargs)
+
+
+def realize_lexeme(term: Lexeme, paradigm: Paradigm) -> list[Forme]:
+    """
+    :param term: Lexeme
+    :param paradigm: Structure encodant les gloses et les blocs d'un langage
+    :return: les formes réalisables d'un lexème à partir d'un paradigme
+    """
+    sigmas: list[frozendict] = paradigm.gloses[term.pos]
+    blocks: list[list[type_Morpheme]] = paradigm.blocks[term.pos]
+
+    formes: list[Forme] = []
+
+    for i_sigma in sigmas:
+        if term.sigma.items() <= i_sigma.items():
+            formes.append(Forme(pos=term.pos,
+                                morphemes=[ruler_radical(rule="", sigma=i_sigma, stems=term.stem),
+                                           *select_morphemes(i_sigma, blocks)],
+                                sigma=i_sigma,
+                                traduction=None))
+    return formes
+
+
+
+def realize_forme(term: Forme, phonology: Phonology) -> str:
+    """
+    Réalisation de la Forme par concaténation de la réalisation de tous ses morphèmes.
+    :param term: Une Forme
+    :param phonology: le DataClass encodant la phonologie
+    :return: une forme réalisée sous forme de chaîne de caractères.
+    """
+    result = ""
+    for morpheme in term.morphemes:
+        assert morpheme.sigma is not None, morpheme
+        m_sigma = dict(term.sigma)
+        m_sigma.update(dict(morpheme.sigma))
+        term.sigma = frozendict(m_sigma)
+        result = realize(term=morpheme,
+                         accumulator=result,
+                         phonology=phonology)
+    return result
+
+
+def realize_suffix(term: Suffix, accumulator: str) -> str:
+    """
+    :param term:
+    :param accumulator:
+    :return: Réalisation d'un suffixe
+    """
+    assert term.rule is not None
+    return f"{accumulator}{term.rule.group(1)}"
+
+
+def realize_prefix(term: Prefix, accumulator: str) -> str:
+    """
+    :param term:
+    :param accumulator:
+    :return: Réalisation d'un préfixe
+    """
+    assert term.rule is not None
+    return f"{term.rule.group(1)}{accumulator}"
+
+
+def realize_circumfix(term: Circumfix, accumulator: str) -> str:
+    """
+
+    :param term:
+    :param accumulator:
+    :return: Réalisation d'un circonfixe
+    """
+    assert term.rule is not None
+    return f"{term.rule.group(1)}{accumulator}{term.rule.group(2)}"
+
+
+def realize_gabarit(term: Gabarit, accumulator: str, phonology: Phonology) -> str:
+    """
+
+    :param term:
+    :param accumulator:
+    :param phonology:
+    :return: Réalisation d'un gabarit
+    """
+    assert term.rule is not None
+    return apply(rule=term.rule.string,
+                 stem=format_stem(accumulator, phonology),
+                 phonology=phonology)
+
+
+def realize_radical(term: Radical, **kwargs) -> str | tuple[str, ...]:
+    """
+    :param term:
+    :param kwargs:
+    :return: Réalisation d'un radical
+    """
+    match term.stem:
+        case str():
+            return term.stem
+        case [first]:
+            return first
+        case tuple():
+            return term.stem
         case _:
             raise TypeError()
+
+
+def realize_selection(term: Selection, accumulator: str) -> str:
+    """
+    :param term:
+    :param accumulator:
+    :return: Choix du stem dans l'espace thématique
+    """
+    assert term.rule is not None
+    return accumulator[int(term.rule.group(1)) - 1]
+
+
+def realize_condition(term: Condition, accumulator: str) -> str:
+    """
+    :param term:
+    :param accumulator:
+    :return: Si cond alors la réalisation de true else la réalisation de false
+    """
+    try:
+        _ = realize(term=term.cond, accumulator=accumulator)
+    except IndexError:
+        return realize(term=term.false, accumulator=accumulator)
+    return realize(term=term.true, accumulator=accumulator)
--- lexique/test/test_realizer.py
+++ lexique/test/test_realizer.py
@@ -1,124 +1,45 @@
-import re
-from typing import Callable, Literal
-
 import pytest
-import yaml
-from frozendict import frozendict  # type: ignore
+from frozendict import frozendict
 
-from lexique import get_lexique_path
-from lexique import etl
-from lexique.realizer import realize
-from lexique.ruler import ruler
-from lexique.structures import Prefix, Suffix, Circumfix, Radical, Forme, Lexeme, Phonology
+from lexique.realizer import realize_lexeme, realize_forme
+from lexique.ruler import ruler_suffix, ruler_radical
+from lexique.structures import Lexeme, Paradigm, Forme, Phonology
 
 
-@pytest.fixture()
-def fx_phonology() -> Phonology:
-    with open("/home/korantin/Documents/Kalaba/lexique/data/kalaba/2/Phonology.yaml") as f:
-        data = yaml.load(f, yaml.Loader)
-        return Phonology(
-            frozendict(data["apophonies"]),
-            frozendict(data["derives"]),
-            frozendict(data["mutations"]),
-            frozenset(data["consonnes"]),
-            frozenset(data["voyelles"]),
-        )
-
-
-@pytest.fixture(scope="module")
-def fx_gloses2() -> tuple[dict[Literal["source", "destination"], frozendict],
-                          dict[Literal["source", "destination"], frozendict]]:
-    with open(f"{get_lexique_path()}/test/data_for_test/avec_traduction/Gloses.yaml", mode="r", encoding="utf8") as f:
-        load = yaml.load(f, Loader=yaml.Loader)
-        return etl.read_glose(glose=load)
-
-
-@pytest.fixture(scope="module")
-def fx_blocks2(fx_gloses2) -> dict[str, dict]:
-    with open(f"{get_lexique_path()}/test/data_for_test/avec_traduction/Blocks.yaml") as f:
-        return etl.read_blocks(data=yaml.load(f, Loader=yaml.Loader),
-                               att_vals=fx_gloses2[1],
-                               voyelles=frozenset())
-
-
-@pytest.fixture()
-def fx_paradigm2(fx_gloses2, fx_blocks2) -> dict[str, dict[frozendict, Callable[[Lexeme], Forme]]]:
-    return etl.build_paradigm(glose=fx_gloses2[0],
-                              blocks=fx_blocks2)
-
-
-@pytest.mark.parametrize("term, accumulator, expected", [
-    (Prefix(rule=re.compile(r"(\w+)\+X").fullmatch("e+X"), sigma=frozendict({"genre": "m"})),
-     "qqch",
-     "eqqch"),
-    (Suffix(rule=re.compile(r"X\+(\w+)").fullmatch("X+e"), sigma=frozendict({"genre": "m"})),
-     "qqch",
-     "qqche"),
-    (Circumfix(rule=re.compile(r"(\w+)\+X\+(\w+)").fullmatch("e+X+e"), sigma=frozendict({"genre": "m"})),
-     "qqch",
-     "eqqche"),
-    # (Gabarit(rule=re.compile(r"(\w+)\+X\+(\w+)").fullmatch("e+X+e"), sigma=frozendict()), "", ""),
-    (Radical(stem="banane", rule=None, sigma=frozendict()), "", "banane"),
-
-    (Radical(stem=("banane",), rule=None, sigma=frozendict()), "", "banane"),
-
-    (Radical(stem=("banane", "bananes"), rule=None, sigma=frozendict()), "", ("banane", "bananes")),
-
-    (Forme(pos="N",
-           morphemes=[
-               Radical(stem=("qqch",), rule=None, sigma=frozendict()),
-               Prefix(rule=re.compile(r"(\w+)\+X").fullmatch("i+X"), sigma=frozendict({"genre": "m"})),
-               Prefix(rule=re.compile(r"(\w+)\+X").fullmatch("a+X"), sigma=frozendict({"genre": "m"})),
-               Circumfix(rule=re.compile(r"(\w+)\+X\+(\w+)").fullmatch("e+X+e"), sigma=frozendict({"genre": "m"}))],
-           sigma=frozendict(), traduction=None),
-     None,
-     "eaiqqche"),
-
-    # (Lexeme(stem="banane", pos="N", sigma=frozendict(), traduction=None)),
-    # (Lexeme(stem="banane", pos="N", sigma=frozendict(), traduction=Lexeme(stem=,pos=,sigma=,traduction=))),
-    #
-    # (Lexeme(stem=("banane",), pos="N", sigma=frozendict(), traduction=None)),
-    # (Lexeme(stem=("banane",), pos="N", sigma=frozendict(), traduction=)),
-    #
-    # (Lexeme(stem=("banane", "bananes"), pos="N", sigma=frozendict(), traduction=None)),
-    # (Lexeme(stem=("banane", "bananes"), pos="N", sigma=frozendict(), traduction=)),
+@pytest.mark.parametrize("lexeme, paradigm, expected", [
+    (Lexeme(stem="banane", pos="N", sigma=frozendict(Genre="m"), traduction=None),
+     Paradigm(gloses={"N": [frozendict(Genre="m", Nombre="sg"), frozendict(Genre="m", Nombre="pl"),
+                            frozendict(Genre="f", Nombre="sg"), frozendict(Genre="f", Nombre="pl")]},
+              blocks={"N": [[ruler_suffix(rule="X+s", sigma=frozendict(Nombre="pl"))]]}),
+     [Forme(pos="N", morphemes=[ruler_radical(rule="", sigma=frozendict(Genre="m", Nombre="sg"), stems="banane")],
+            sigma=frozendict(Genre="m", Nombre="sg"), traduction=None),
+      Forme(pos="N", morphemes=[ruler_suffix(rule="X+s", sigma=frozendict(Nombre="pl"))],
+            sigma=frozendict(Genre="m", Nombre="pl"), traduction=None)])
 ])
-def test_realize_(phonology, term, accumulator, expected) -> None:
-    actual = realize(term=term, accumulator=accumulator, phonology=phonology)
+def test_realize_lexeme(lexeme, paradigm, expected) -> None:
+    actual = realize_lexeme(lexeme, paradigm)
+    for i_idx in range(len(actual)):
+        assert actual[i_idx].pos == expected[i_idx].pos
+        for j_idx in range(len(actual[i_idx].morphemes)):
+            assert actual[i_idx].morphemes[j_idx].rule.string == expected[i_idx].morphemes[j_idx].rule.string
+        assert actual[i_idx].sigma == expected[i_idx].sigma
+        assert actual[i_idx].traduction == expected[i_idx].traduction
+
+
+@pytest.mark.parametrize("forme, phonology, expected", [
+    (Forme(pos="N", morphemes=[ruler_radical(rule="",
+                                             sigma=frozendict(Genre="m",
+                                                              Nombre="sg"),
+                                             stems="banane")],
+           sigma=frozendict(Genre="m", Nombre="sg"),
+           traduction=None),
+     Phonology(consonnes=frozenset("zrtpmkl"),
+               voyelles=frozenset("aeiou"),
+               mutations=frozendict(a="b"),
+               derives=frozendict(a="b"),
+               apophonies=frozendict(a="b")),
+     "banane")
+])
+def test_realize_forme(forme, phonology, expected):
+    actual = realize_forme(forme, phonology)
     assert actual == expected
-
-
-@pytest.mark.skipif(reason="le paradigme n'a pas de restriction, du coup la combinatoire est trop grande pour le test")
-@pytest.mark.parametrize("term, expected", [
-    (Lexeme(stem=("petit",), pos="ADJ", sigma=frozendict(),
-            traduction=Lexeme(stem="petit", pos="ADJ", sigma=frozendict(), traduction=None)),
-     ['petitiv', 'petitav', 'petitov', 'petitif', 'petitaf', 'petitof', 'petitid', 'petitad', 'petitod', 'petitik',
-      'petitak', 'petitok', 'petitom', 'petitam', 'petitim', 'petitov', 'petitav', 'petitiv', 'petitot', 'petitat',
-      'petitit', 'petitog', 'petitag', 'petitig']),
-
-    (Lexeme(stem=("petit",), pos="ADJ", sigma=frozendict({"Genre": "A"}),
-            traduction=Lexeme(stem="petit", pos="ADJ", sigma=frozendict(), traduction=None)),
-     ['petitiv', 'petitav', 'petitov', 'petitom', 'petitam', 'petitim']),
-
-    (Lexeme(stem=("épousailles",), pos="N", sigma=frozendict({"Genre": "B", "Nombre": "Pl"}),
-            traduction=Lexeme(stem="petit", pos="ADJ", sigma=frozendict(), traduction=None)),
-     ['reposowula', 'boposowula', 'koposowula', 'liposowula']),
-])
-def test_realize_lexeme(fx_paradigm2, phonology, term, expected) -> None:
-    actual = realize(term=term, paradigm=fx_paradigm2)
-    assert [realize(term=forme, phonology=phonology) for forme in actual] == expected
-
-
-@pytest.mark.parametrize("forme", [
-    Forme(pos='N',
-          morphemes=[Radical(rule=None, sigma=frozendict(), stem=("padaN",)),
-                     ruler(rule="1a4A2V3e", sigma=frozendict({'nombre': 'sg'}), voyelles=frozenset("aeiou"))],
-          sigma=frozendict({'genre': 'm', 'nombre': 'sg', 'þgenre': 'þm', 'þnombre': 'þsg'}),
-          traduction=Forme(pos='N',
-                           morphemes=[Radical(rule=None, sigma=frozendict({'genre': 'f'}), stem=('plaine',))],
-                           sigma=frozendict({'genre': 'm', 'nombre': 'sg', 'þgenre': 'þm', 'þnombre': 'þsg'}),
-                           traduction=None))
-])
-def test_realize(forme, fx_phonology) -> None:
-    assert realize(term=forme, phonology=fx_phonology) == "papudaNe"
-    assert realize(term=forme.traduction, phonology=fx_phonology) == "plaine"
