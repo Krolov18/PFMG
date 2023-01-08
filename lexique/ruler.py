a5501bae3d054e61fba95beab2cdd5c23ac67ca3
Krolov18 <korantin.leveque@protonmail.com>
2023-01-08 13:23:11 +0100

    Met à jour ruler.py

--- lexique/ruler.py
+++ lexique/ruler.py
@@ -1,44 +1,130 @@
 import re
-from typing import Literal, Pattern
+from enum import Enum
 
 from frozendict import frozendict  # type: ignore
 
-# from lexique.errors import Errors
 from lexique.structures import Prefix, Suffix, Circumfix, Gabarit, Condition, Selection
-from lexique.structures import Morpheme
+from lexique.types_for_kalaba import type_Sigma
+from utils.abstract_factory import factory_function
 from utils.functions import static_vars
 
-
-# from utils.abstract_factory import factory_function
-# from utils.functions import static_vars
+type_Morpheme = Prefix | Suffix | Circumfix | Gabarit | Condition | Selection
 
 
-@static_vars(rules={"prefix": re.compile(r"^(.*)\+X$"),
-                    "suffix": re.compile(r"^X\+(.*)$"),
-                    "circumfix": re.compile(r"^([^+]*)\+X\+([^+]*)$"),
-                    "selection": re.compile(r"^X(\d+)$"),
-                    "condition": re.compile(r"^(.*)\?(.*):(.*)$")})
-def ruler(rule: str, sigma: frozendict, voyelles: frozenset) -> Morpheme:
-    attr = getattr(ruler, "rules")
-    if voyelles and ("gabarit" not in attr):
-        attr["gabarit"] = re.compile(fr"^([{''.join(voyelles)}AUV1-9]){{4,9}}$")
+class Ruler(Enum):
+    """
+    Attibutes :
+    -----------
+    :param PREFIX :
+    :param SUFFIX :
+    :param CIRCUMFIX :
+    :param GABARIT :
+    :param SELECTION :
+    :param CONDITION :
+    """
+    PREFIX = "prefix"
+    SUFFIX = "suffix"
+    CIRCUMFIX = "circumfix"
+    GABARIT = "gabarit"
+    SELECTION = "selection"
+    CONDITION = "condition"
 
-    match rule:
-        case r if i_rule := attr["prefix"].fullmatch(r):
-            return Prefix(rule=i_rule, sigma=sigma)
-        case r if i_rule := attr["suffix"].fullmatch(r):
-            return Suffix(rule=i_rule, sigma=sigma)
-        case r if i_rule := attr["circumfix"].fullmatch(r):
-            return Circumfix(rule=i_rule, sigma=sigma)
-        case r if i_rule := attr["selection"].fullmatch(r):
-            return Selection(rule=i_rule, sigma=sigma)
-        case r if i_rule := attr["gabarit"].fullmatch(r):
-            return Gabarit(rule=i_rule, sigma=sigma)
-        case r if i_rule := attr["condition"].fullmatch(r):
-            return Condition(rule=i_rule,
-                             sigma=sigma,
-                             cond=ruler(rule=i_rule.group(1), sigma=sigma, voyelles=voyelles),
-                             true=ruler(rule=i_rule.group(2), sigma=sigma, voyelles=voyelles),
-                             false=ruler(rule=i_rule.group(3), sigma=sigma, voyelles=voyelles))
-        case _:
-            raise ValueError()
+
+def ruler(id_ruler: Ruler, rule: str, sigma: type_Sigma) -> type_Morpheme:
+    """
+    :param id_ruler: Identifiant possible pour une règle d'affixation
+    :param rule: La règle en question
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: Le morphème prêt à être utilisé
+    """
+    return factory_function(concrete_product=f"ruler_{id_ruler.value}",
+                            package=__name__,
+                            rule=rule,
+                            sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^(.*)\+X$"))
+def ruler_prefix(rule: str, sigma: type_Sigma) -> Prefix:
+    """
+    :param rule: Règle préfixale
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: un Préfix
+    """
+    assert (i_rule := getattr(ruler_prefix, "REG").fullmatch(rule)) is not None
+    return Prefix(rule=i_rule, sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^X\+(.*)$"))
+def ruler_suffix(rule: str, sigma: type_Sigma) -> Suffix:
+    """
+    :param rule: Règle suffixale
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: un Suffix
+    """
+    assert (i_rule := getattr(ruler_suffix, "REG").fullmatch(rule)) is not None
+    return Suffix(rule=i_rule, sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^([^+]*)\+X\+([^+]*)$"))
+def ruler_circumfix(rule: str, sigma: type_Sigma) -> Circumfix:
+    """
+    :param rule: Règle circonffixale
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: un Circumfix
+    """
+    assert (i_rule := getattr(ruler_circumfix, "REG").fullmatch(rule)) is not None
+    return Circumfix(rule=i_rule, sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^X(\d+)$"))
+def ruler_selection(rule: str, sigma: type_Sigma) -> Selection:
+    """
+    :param rule: Règle sélectionnale
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: un Selection
+    """
+    assert (i_rule := getattr(ruler_selection, "REG").fullmatch(rule)) is not None
+    return Selection(rule=i_rule, sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^(.*)\?(.*):(.*)$"))
+def ruler_condition(rule: str, sigma: type_Sigma) -> Condition:
+    """
+    :param rule: Règle conditionnale
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: une Condition
+    """
+    assert (i_rule := getattr(ruler_condition, "REG").fullmatch(rule)) is not None
+    return Condition(rule=i_rule,
+                     cond=ruler_selection(rule=i_rule.group(1), sigma=sigma),
+                     true=ruler_selection(rule=i_rule.group(2), sigma=sigma),
+                     false=ruler_selection(rule=i_rule.group(3), sigma=sigma),
+                     sigma=sigma)
+
+
+@static_vars(REG=re.compile(r"^[iueoaAUV1-9]{4,9}$"))
+def ruler_gabarit(rule: str, sigma: type_Sigma) -> Gabarit:
+    """
+    :param rule: Règle gabaritique
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: un Gabarit
+    """
+    assert (i_rule := getattr(ruler_gabarit, "REG").fullmatch(rule)) is not None
+    return Gabarit(rule=i_rule, sigma=sigma)
+
+
+def ruler_any(rule: str, sigma: type_Sigma) -> type_Morpheme:
+    """
+    Parcourt chaucune des règles disponibles puis renvoie la première qui matche.
+    :param rule: Une règle parmi toutes celles possibles
+    :param sigma: Un ensemble de traits/valeurs pour cette règle
+    :return: une Selection
+    """
+    for id_ruler in Ruler:
+        try:
+            return ruler(id_ruler=id_ruler,
+                         rule=rule,
+                         sigma=sigma)
+        except AssertionError:
+            continue
+    raise ValueError()
--- lexique/test/test_ruler.py
+++ lexique/test/test_ruler.py
@@ -1,80 +1,104 @@
 import pytest
 from frozendict import frozendict
 
-from lexique.realizer import realize
-from lexique.ruler import ruler
-from lexique.structures import Phonology
+from lexique.ruler import (ruler_prefix, ruler_suffix, ruler_circumfix, ruler_condition,
+                           ruler_selection, ruler_gabarit, ruler_any)
+from lexique.structures import Condition, Selection, Prefix, Suffix, Circumfix, Gabarit
 
 
-@pytest.fixture(scope="module")
-def phonology() -> Phonology:
-    return Phonology(mutations=frozendict({'p': 'p', 't': 'p', 'k': 't', 'b': 'p', 'd': 'b', 'g': 'd',
-                                           'm': 'm', 'n': 'm', 'N': 'n', 'f': 'f',
-                                           's': 'f', 'S': 's', 'v': 'f', 'z': 'v', 'Z': 'z', 'r': 'w', 'l': 'r',
-                                           'j': 'w', 'w': 'w'}),
-                     apophonies=frozendict({'Ø': 'i', 'i': 'a', 'a': 'u', 'u': 'u', 'e': 'o', 'o': 'o'}),
-                     derives=frozendict({'A': 'V', 'D': 'C'}),
-                     consonnes=frozenset("ptkbdgmnNfsSvzZrljw"),
-                     voyelles=frozenset("iueoa"))
+@pytest.mark.parametrize("rule, sigma", [
+    ("e+X", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_prefix(rule, sigma) -> None:
+    actual = ruler_prefix(rule=rule, sigma=sigma)
+    assert isinstance(actual, Prefix)
+    assert actual.rule is not None
+    assert actual.rule.group(1) == rule[0]
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("rule, sigma, expected", [
-    ("e+X", frozendict(genre="m", nombre="sg"), "eRADICAL"),
-    ])
-def test_ruler_prefix(rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator="RADICAL") == expected
+@pytest.mark.parametrize("rule, sigma", [
+    ("X+e", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_suffix(rule, sigma) -> None:
+    actual = ruler_suffix(rule=rule, sigma=sigma)
+    assert isinstance(actual, Suffix)
+    assert actual.rule is not None
+    assert actual.rule.group(1) == rule[-1]
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("rule, sigma, expected", [
-    ("X+e", frozendict(genre="m", nombre="sg"), "RADICALe"),
-    ])
-def test_ruler_suffix(rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator="RADICAL") == expected
+@pytest.mark.parametrize("rule, sigma", [
+    ("e+X+a", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_circumfix(rule, sigma) -> None:
+    actual = ruler_circumfix(rule=rule, sigma=sigma)
+    assert isinstance(actual, Circumfix)
+    assert actual.rule is not None
+    assert actual.rule.group(1) == rule[:rule.find("+")]
+    assert actual.rule.group(2) == rule[rule.rfind("+") + 1:]
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("rule, sigma, expected", [
-    ("e+X+e", frozendict(genre="m", nombre="sg"), "eRADICALe"),
-    ])
-def test_ruler_circumfix(rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator="RADICAL") == expected
+@pytest.mark.parametrize("rule, sigma", [
+    ("i4A1o2a3V", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_gabarit(phonology, rule, sigma) -> None:
+    actual = ruler_gabarit(rule=rule, sigma=sigma)
+    assert isinstance(actual, Gabarit)
+    assert actual.rule is not None
+    assert actual.rule.group(0) == rule
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("rule, sigma, expected", [
-    ("i4A1o2a3V", frozendict(genre="m", nombre="sg"), "iwarobani"),
-    ])
-def test_ruler_gabarit(phonology, rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator="rbin", phonology=phonology) == expected
+@pytest.mark.parametrize("rule, sigma", [
+    ("X1", frozendict(genre="m", nombre="sg")),
+    ("X2", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_selection(rule, sigma) -> None:
+    actual = ruler_selection(rule=rule, sigma=sigma)
+    assert isinstance(actual, Selection)
+    assert actual.rule is not None
+    assert int(actual.rule.group(1)) == int(rule[1:])
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("accumulator, rule, sigma, expected", [
-    (("souris",), "X1", frozendict(genre="m", nombre="sg"), "souris"),
-    (("blanc", "blanche"), "X2", frozendict(genre="m", nombre="sg"), "blanche"),
-    ])
-def test_ruler_selection(accumulator, rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator=accumulator) == expected
+@pytest.mark.parametrize("rule, sigma", [
+    ("X2?X2:X1", frozendict(genre="m", nombre="sg")),
+    ("X2?X2:X1", frozendict(genre="m", nombre="sg")),
+])
+def test_ruler_condition(rule, sigma) -> None:
+    actual = ruler_condition(rule=rule, sigma=sigma)
+    assert isinstance(actual, Condition)
+    assert actual.rule is not None
+    assert actual.rule[0] == rule
+    assert actual.rule[1] == rule[:rule.find("?")]
+    assert actual.rule[2] == rule[rule.find("?") + 1:rule.find(":")]
+    assert actual.rule[3] == rule[rule.find(":") + 1:]
+    assert actual.sigma == sigma
 
 
-@pytest.mark.parametrize("accumulator, rule, sigma, expected", [
-    (("souris",), "X2?X2:X1", frozendict(genre="m", nombre="sg"), "souris"),
-    (("chat", "chatte"), "X2?X2:X1", frozendict(genre="m", nombre="sg"), "chatte"),
-    ])
-def test_ruler_ternary(accumulator, rule, sigma, expected) -> None:
-    actual = ruler(rule=rule, sigma=sigma, voyelles=frozenset("aeiou"))
-    assert realize(actual, accumulator=accumulator) == expected
+@pytest.mark.parametrize("rule, expected", [
+    ("e+X", Prefix),
+    ("X+e", Suffix),
+    ("a+X+e", Circumfix),
+    ("i4A1o2a3V", Gabarit),
+    ("X4", Selection),
+    ("X2?X2:X1", Condition)
+])
+def test_rule_any(rule, expected) -> None:
+    actual = ruler_any(rule=rule, sigma=frozendict(genre="m", nombre="sg"))
+    assert isinstance(actual, expected)
 
 
-@pytest.mark.parametrize("rule", [
-    # "X2:",
-    # ":X2",
-    "X2?X1?X2",
-    ])
-def test_ruler_all_errors(rule) -> None:
+@pytest.mark.parametrize("rule, expected", [
+    ("eX", Prefix),
+    ("Xe", Suffix),
+    ("a+X+e+9", Circumfix),
+    ("i4A1o2a3V3R", Gabarit),
+    ("X", Selection),
+    ("X2X2:X1", Condition)
+])
+def test_rule_any_errors(rule, expected) -> None:
     with pytest.raises(ValueError):
-        _ = ruler(rule=rule,
-                  sigma=frozendict(),
-                  voyelles=frozenset("aeiou"))
+        _ = ruler_any(rule=rule, sigma=frozendict(genre="m", nombre="sg"))
