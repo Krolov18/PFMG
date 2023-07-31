96186523ca03fa2aff3395ce2e80177835217247
korantin <korantin.leveque@gmail.com>
2023-08-01 00:15:35 +0200

    Supprime fichier qui n'est plus utilisé

--- lexique_2/lexical_structures/types.py
+++
@@ -1,8 +0,0 @@
-from lexique_2.lexical_structures.Prefix import Prefix
-from lexique_2.lexical_structures.Suffix import Suffix
-from lexique_2.lexical_structures.Circumfix import Circumfix
-from lexique_2.lexical_structures.Condition import Condition
-from lexique_2.lexical_structures.Selection import Selection
-from lexique_2.lexical_structures.Gabarit import Gabarit
-
-type_morpheme = Prefix | Suffix | Circumfix | Selection | Gabarit | Condition
