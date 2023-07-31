277d79d27965db9d8ccce04ae121a20b663cea09
korantin <korantin.leveque@gmail.com>
2023-08-01 00:03:19 +0200

    Regroupe les raccourcis

--- utils/paths.py
+++ utils/paths.py
@@ -8,3 +8,7 @@ def get_project_path() -> Path:
 
 def get_validation_file_path(project_path: Path = get_project_path()) -> Path:
     return project_path / "schemas"
+
+
+def get_data_test_grammar() -> Path:
+    return Path(__file__).parent / "test" / "data_for_test"
