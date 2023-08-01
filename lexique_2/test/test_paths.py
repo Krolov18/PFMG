0e77c63713ced7bcb685593e57630e7f6a833472
korantin <korantin.leveque@gmail.com>
2023-08-01 10:14:58 +0200

    Supprime fichier de test

--- lexique_2/test/test_paths.py
+++
@@ -1,5 +0,0 @@
-def test_data_test_grammar():
-    from utils.paths import get_data_test_grammar
-    assert get_data_test_grammar().exists()
-    assert get_data_test_grammar().is_dir()
-    assert get_data_test_grammar().name == "data_for_test"
