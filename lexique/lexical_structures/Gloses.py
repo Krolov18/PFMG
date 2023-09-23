f113e1cfd6c11c1d83265e845f710db28df6c18a
Krolov18 <korantin.leveque@protonmail.com>
2023-09-24 00:22:41 +0200

    Refactore Gloses pour recevoir source et destination

--- lexique/lexical_structures/Gloses.py
+++ lexique/lexical_structures/Gloses.py
@@ -1,49 +1,55 @@
 from dataclasses import dataclass
-import itertools as it
+from itertools import product
 from pathlib import Path
-from typing import Iterator, overload
+from typing import Iterator, Literal, overload
 
 import yaml
 from frozendict import frozendict
 
 from lexique.lexical_structures.interfaces.Reader import Reader
-from lexique.lexical_structures.interfaces.Searchable import Searchable
 
 d_grid = dict[str, list[str]]
 l_grid = list[dict[str, list[str]]]
 d_or_l_grid = d_grid | l_grid
 
+SubGlose = dict[str, list[frozendict]]
+
 
 @dataclass
-class Gloses(Reader, Searchable):
-    """
-    Structure contenant les informations morphosyntaxiques
-    par catégorie grammaticale.
-    Attributes :
-        data : membre contenant la structure
-    """
-    data: dict[str, list[frozendict]]
+class Gloses(Reader):
+    source: SubGlose
+    destination: SubGlose
+
+    def __post_init__(self):
+        self.struct = {k: [dict(zip(vars(self).keys(), sigma))
+                           for sigma in
+                           product(self.source[k], self.destination[k])]
+                       for k in self.source.keys()
+                       if k in self.source and k in self.destination}
+
+    def __call__(
+        self,
+        pos: str
+    ) -> list[dict[Literal["source", "destination"], str]]:
+        return self.struct[pos]
 
     @classmethod
-    # @cache
     def from_disk(cls, path: Path) -> 'Gloses':
-        """
-        Cette méthode de classe est décorée de 'cache'
-        afin de rendre cette méthode "comme" un singleton
-        Si cette méthode est appelée plusieurs fois avec
-        le même paramètre alors cette fonction sera exécutée
-        la première fois, puis récupérée en cache les autres fois.
-        :param path:
-        :return:
-        """
         assert path.name.endswith("Gloses.yaml")
         with open(path, mode="r", encoding="utf8") as file_handler:
             data: dict[str, dict[str, list[str]]] = yaml.load(
                 file_handler,
                 Loader=yaml.Loader
             )
-        return cls(data={category: list(cls.__gridify(att_vals))
-                         for category, att_vals in data.items()})
+        source = {category: list(cls.__gridify(att_vals))
+                  for category, att_vals in data["source"].items()}
+        destination = {category: list(cls.__gridify(att_vals))
+                       for category, att_vals in data["destination"].items()}
+
+        return cls(
+            source=source,
+            destination=destination
+        )
 
     @staticmethod
     @overload
@@ -62,7 +68,8 @@ class Gloses(Reader, Searchable):
                      ou liste de dictionnaires Attribut -> [Valeurs]
         :return: générateur de Gloses
         """
-        method_name: str = f"_{Gloses.__name__}__gridify_{grid.__class__.__name__.lower()}"
+        method_name: str = (f"_{Gloses.__name__}__gridify_"
+                            f"{grid.__class__.__name__.lower()}")
         return getattr(Gloses, method_name)(grid=grid)
 
     @staticmethod
@@ -80,9 +87,11 @@ class Gloses(Reader, Searchable):
             return frozendict()
 
         keys, values = zip(*items)
-        for value in it.product(*values):
-            yield frozendict([*zip(keys, value),
-                              *zip(value, keys)])
+        for value in product(*values):
+            yield frozendict(
+                [*zip(keys, value),
+                 *zip(value, keys)]
+            )
 
     @staticmethod
     def __gridify_list(grid: l_grid) -> Iterator[frozendict]:
@@ -96,25 +105,3 @@ class Gloses(Reader, Searchable):
             return
         for i_grid in grid:
             yield from Gloses.__gridify_dict(i_grid)
-
-    def search(self, pos: str, value: str) -> str | None:
-        """
-        Méthode qui permet de rechercher une glose
-        à partir d'une catégorie grammaticale et d'une valeur.
-        :param pos: Catégorie grammaticale.
-        :param value: Valeur recherchée.
-        :return: Glose correspondante.
-        """
-        if (result := next((sigma[value]
-                            for sigma in self.data[pos]
-                            if value in sigma), None)) is None:
-            raise ValueError(pos, value)
-        return result
-
-    def is_pos(self, pos: str) -> bool:
-        """
-        Méthode qui permet de savoir si une catégorie grammaticale est présente dans la structure.
-        :param pos: Catégorie grammaticale.
-        :return: Présence de la catégorie grammaticale.
-        """
-        return pos in self.data
--- lexique/lexical_structures/test/test_gloses.py
+++ lexique/lexical_structures/test/test_gloses.py
@@ -4,70 +4,91 @@ from frozendict import frozendict
 
 from lexique.lexical_structures.Gloses import Gloses
 
+parametrize = pytest.mark.parametrize("gloses, expected", [
 
-@pytest.mark.parametrize("gloses, expected", [
-    ({"N": {"Genre": ["m"]}},
-     {"N": [frozendict(Genre="m", m="Genre")]}),
+    ({"source": {"N": {"Genre": ["m"]}},
+      "destination": {"N": {"Genre": ["m"]}}},
+     {"source": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'})]},
+      "destination": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'})]}}),
 
-    ({"N": {"Genre": ["m", "f"]}},
-     {"N": [frozendict(Genre="m", m="Genre"),
-            frozendict(Genre="f", f="Genre")]}),
+    ({"source": {"N": {"Genre": ["m", "f"]}},
+      "destination": {"N": {"Genre": ["m", "f"]}}},
+     {"source": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'}),
+                       frozendict({'Genre': 'f', 'f': 'Genre'})]},
+      "destination": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'}),
+                            frozendict({'Genre': 'f', 'f': 'Genre'})]}}),
 
-    ({"N": {"Genre": ["m", "f"],
-            "Nombre": ["sg", "pl"]}},
-     {"N": [frozendict(Genre="m", Nombre="sg", m="Genre", sg="Nombre"),
-            frozendict(Genre="m", Nombre="pl", m="Genre", pl="Nombre"),
-            frozendict(Genre="f", Nombre="sg", f="Genre", sg="Nombre"),
-            frozendict(Genre="f", Nombre="pl", f="Genre", pl="Nombre")]}),
-
-    ({"N": {"Genre": ["m", "f"],
-            "Nombre": ["sg", "pl"]},
-      "A": {"Cas": ["erg"]}},
-     {"N": [frozendict(Genre="m", Nombre="sg", m="Genre", sg="Nombre"),
-            frozendict(Genre="m", Nombre="pl", m="Genre", pl="Nombre"),
-            frozendict(Genre="f", Nombre="sg", f="Genre", sg="Nombre"),
-            frozendict(Genre="f", Nombre="pl", f="Genre", pl="Nombre")],
-      "A": [frozendict(Cas="erg", erg="Cas")]}),
-
-    ({"N": [{"Genre": "m"}]},
-     {"N": [frozendict(Genre="m", m="Genre")]}),
-
-    ({"N": []},
-     {"N": [[]]}),
-
-    ({"N": [{}]}, {"N": []})
-
+    ({"source": {"N": {"Genre": ["m", "f"], "Nombre": ["sg", "pl"]}},
+      "destination": {"N": {"Cas": ["nom", "acc", "dat"]}}},
+     {"source": {"N": [frozendict({'Genre': 'm', 'm': 'Genre',
+                                   "Nombre": "sg", "sg": "Nombre"}),
+                       frozendict({'Genre': 'm', 'm': 'Genre',
+                                   "Nombre": "pl", "pl": "Nombre"}),
+                       frozendict({'Genre': 'f', 'f': 'Genre',
+                                   "Nombre": "sg", "sg": "Nombre"}),
+                       frozendict({'Genre': 'f', 'f': 'Genre',
+                                   "Nombre": "pl", "pl": "Nombre"})]},
+      "destination": {"N": [frozendict({'Cas': 'nom', 'nom': 'Cas'}),
+                            frozendict({'Cas': 'acc', 'acc': 'Cas'}),
+                            frozendict({'Cas': 'dat', 'dat': 'Cas'})]}}),
 ])
+
+
+@parametrize
 def test_gloses_from_disk(tmp_path, gloses, expected) -> None:
     gloses_path = tmp_path / "Gloses.yaml"
     with open(gloses_path, mode="w", encoding="utf8") as file_handler:
         yaml.dump(gloses, file_handler)
     actual = Gloses.from_disk(gloses_path)
 
-    assert actual == Gloses(data=expected)
+    assert actual == Gloses(**expected)
 
 
-def test_gloses_search(tmp_path) -> None:
-    gloses_path = tmp_path / "Gloses.yaml"
-    with open(gloses_path, mode="w", encoding="utf8") as file_handler:
-        yaml.dump({"N": []}, file_handler)
-    with pytest.raises(ValueError):
-        _ = Gloses.from_disk(gloses_path).search("N", "Genre")
+parametrize = pytest.mark.parametrize("params, expected", [
 
-    gloses_path = tmp_path / "Gloses.yaml"
-    with open(gloses_path, mode="w", encoding="utf8") as file_handler:
-        yaml.dump({"N": [{"Genre": "m"}]}, file_handler)
-    assert Gloses.from_disk(gloses_path).search("N", "Genre") == "m"
+    ({"source": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'})]},
+      "destination": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'})]}},
+     [{"source": frozendict({'Genre': 'm', 'm': 'Genre'}),
+       "destination": frozendict({'Genre': 'm', 'm': 'Genre'})}]),
 
-    gloses_path = tmp_path / "Gloses.yaml"
-    with open(gloses_path, mode="w", encoding="utf8") as file_handler:
-        yaml.dump({"N": [{"Genre": "f"}, {"Genre": "m"}]}, file_handler)
-    assert Gloses.from_disk(gloses_path).search("N", "Genre") == "f"
+    ({"source": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'}),
+                       frozendict({'Genre': 'f', 'f': 'Genre'})]},
+      "destination": {'N': [frozendict({'Genre': 'm', 'm': 'Genre'}),
+                            frozendict({'Genre': 'f', 'f': 'Genre'})]}},
+     [{'destination': frozendict({'Genre': 'm', 'm': 'Genre'}),
+       'source': frozendict({'Genre': 'm', 'm': 'Genre'})},
+      {'destination': frozendict({'Genre': 'f', 'f': 'Genre'}),
+       'source': frozendict({'Genre': 'm', 'm': 'Genre'})},
+      {'destination': frozendict({'Genre': 'm', 'm': 'Genre'}),
+       'source': frozendict({'Genre': 'f', 'f': 'Genre'})},
+      {'destination': frozendict({'Genre': 'f', 'f': 'Genre'}),
+       'source': frozendict({'Genre': 'f', 'f': 'Genre'})}]),
+
+    ({"source": {"N": [frozendict({'Genre': 'm', 'm': 'Genre',
+                                   "Nombre": "sg", "sg": "Nombre"}),
+                       frozendict({'Genre': 'm', 'm': 'Genre',
+                                   "Nombre": "pl", "pl": "Nombre"}),
+                       frozendict({'Genre': 'f', 'f': 'Genre',
+                                   "Nombre": "sg", "sg": "Nombre"}),
+                       frozendict({'Genre': 'f', 'f': 'Genre',
+                                   "Nombre": "pl", "pl": "Nombre"})]},
+      "destination": {"N": [frozendict({'Cas': 'm', 'm': 'Cas'})]}},
+     [{'destination': frozendict({'Cas': 'm', 'm': 'Cas'}),
+       'source': frozendict({'Genre': 'm', 'm': 'Genre',
+                             'Nombre': 'sg', 'sg': 'Nombre'})},
+      {'destination': frozendict({'Cas': 'm', 'm': 'Cas'}),
+       'source': frozendict({'Genre': 'm', 'm': 'Genre',
+                             'Nombre': 'pl', 'pl': 'Nombre'})},
+      {'destination': frozendict({'Cas': 'm', 'm': 'Cas'}),
+       'source': frozendict({'Genre': 'f', 'f': 'Genre',
+                             'Nombre': 'sg', 'sg': 'Nombre'})},
+      {'destination': frozendict({'Cas': 'm', 'm': 'Cas'}),
+       'source': frozendict({'Genre': 'f', 'f': 'Genre',
+                             'Nombre': 'pl', 'pl': 'Nombre'})}]),
+])
 
 
-def test_is_pos(tmp_path) -> None:
-    gloses_path = tmp_path / "Gloses.yaml"
-    with open(gloses_path, mode="w", encoding="utf8") as file_handler:
-        yaml.dump({"N": [{"Genre": "f"}, {"Genre": "m"}]}, file_handler)
-    assert Gloses.from_disk(gloses_path).is_pos("N")
-    assert not Gloses.from_disk(gloses_path).is_pos("A")
+@parametrize
+def test__call__(tmp_path, params, expected) -> None:
+    actual = Gloses(**params)
+    assert actual(pos="N") == expected
