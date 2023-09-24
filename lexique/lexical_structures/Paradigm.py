392a8b80d9aba15e325d2076268ffb627e914936
Krolov18 <korantin.leveque@protonmail.com>
2023-09-24 17:37:47 +0200

    Refactore Paradigm suite à l'intégration de source/destination

--- lexique/lexical_structures/Paradigm.py
+++ lexique/lexical_structures/Paradigm.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Generator
 
 from lexique.lexical_structures.interfaces.Reader import Reader
 from lexique.lexical_structures.Blocks import Blocks
-from lexique.lexical_structures.Forme import Forme, FormeEntry
+from lexique.lexical_structures.Forme import Forme
+from lexique.lexical_structures.FormeEntry import FormeEntry
 from lexique.lexical_structures.Gloses import Gloses
-from lexique.lexical_structures.Lexeme import Lexeme, LexemeEntry
+from lexique.lexical_structures.Lexeme import Lexeme
 from lexique.lexical_structures.Morphemes import Morphemes
 from lexique.lexical_structures.interfaces.Realizer import Realizer
 
@@ -17,44 +19,55 @@ class BlockEntry:
 @dataclass
 class Paradigm(Realizer, Reader):
     """
-    Classe qui permet de réaliser un paradigme.
+    Classe qui permet de réaliser un lexème.
     """
     gloses: Gloses
     blocks: Blocks
 
-    def __realise(self, lexeme: LexemeEntry, blocks: BlockEntry) -> list[FormeEntry]:
-        entries: list[FormeEntry] = list()
-
-        for i_sigma in sigmas:
-            if lexeme.sigma.items() <= i_sigma.items():
-                entries.append(
-                    FormeEntry(
-                        pos=lexeme.pos,
-                        sigma=i_sigma,
-                        morphemes=Morphemes(
-                            radical=lexeme.to_radical(),
-                            others=blocks.select_morphemes(
-                                pos=lexeme.pos,
-                                sigma=i_sigma
-                            )
-                        )
-                    )
-                )
-        return entries
-
-    def realize(self, lexeme: Lexeme) -> list[Forme]:
+    def realize(self, lexeme: Lexeme) -> Generator[Forme, None, None]:
         """
         Méthode qui permet de réaliser un lexème donné.
 
         :param lexeme: Lexème à réaliser.
         :return: Liste des réalisations du lexème.
         """
-        return [Forme(source=s,
-                      destination=d)
-                for s, d in zip(self.__realise(lexeme=lexeme.source,
-                                               blocks=self.blocks.source),
-                                self.__realise(lexeme=lexeme.destination,
-                                               blocks=self.blocks.destination))]
+        gloses = self.gloses(lexeme.source.pos)
+        source_sigma_items = lexeme.source.sigma.items()
+        destination_sigma_items = lexeme.destination.sigma.items()
+        lexeme_pos = lexeme.source.pos
+        for i_sigma in gloses:
+            i_sigma_source = i_sigma["source"]
+            i_sigma_destination = i_sigma["destination"]
+            source_validation = (source_sigma_items <= i_sigma_source.items())
+            destination_validation = (destination_sigma_items
+                                      <= i_sigma_destination.items())
+            if source_validation and destination_validation:
+                forme_entry_source = FormeEntry(
+                    pos=lexeme_pos,
+                    sigma=i_sigma_source,
+                    morphemes=Morphemes(
+                        radical=lexeme.source.to_radical(),
+                        others=self.blocks.source(
+                            pos=lexeme_pos,
+                            sigma=i_sigma_source
+                        )
+                    )
+                )
+                forme_entry_destination = FormeEntry(
+                    pos=lexeme_pos,
+                    sigma=i_sigma_destination,
+                    morphemes=Morphemes(
+                        radical=lexeme.destination.to_radical(),
+                        others=self.blocks.destination(
+                            pos=lexeme_pos,
+                            sigma=i_sigma_destination
+                        )
+                    )
+                )
+                yield Forme(
+                    source=forme_entry_source,
+                    destination=forme_entry_destination
+                )
 
     @classmethod
     def from_disk(cls, path: Path) -> 'Paradigm':
@@ -64,8 +77,12 @@ class Paradigm(Realizer, Reader):
         :param path: Chemin à partir duquel charger le paradigme.
         :return: Instance de Paradigm.
         """
-        return cls(gloses=Gloses.from_disk(
-            path=path / "Gloses.yaml"),
-                   blocks=Blocks.from_disk(
-                       path=path / "Blocks.yaml")
+        assert (path / "Gloses.yaml").exists()
+        return cls(
+            gloses=Gloses.from_disk(
+                path=path / "Gloses.yaml"
+            ),
+            blocks=Blocks.from_disk(
+                path=path / "Blocks.yaml"
+            )
         )
