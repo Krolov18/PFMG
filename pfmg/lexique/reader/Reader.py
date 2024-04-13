6d16e85e3f5bde85350bafead2205f2e6e8dd28e
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-04-13 16:35:08 +0200

    Refactore le package reader/

---
+++ pfmg/lexique/reader/Reader.py
@@ -0,0 +1,16 @@
+"""Reader."""
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+
+class Reader[T](ABC):
+    """Construit un type depuis des infos sur le disque."""
+
+    @classmethod
+    @abstractmethod
+    def from_disk(cls, path: Path) -> T:
+        """Construit un type depuis un fichier sur le disque.
+
+        :param path: Chemin du fichier
+        :return: une instance T
+        """
