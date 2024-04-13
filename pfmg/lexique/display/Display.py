4ba3aee9a6235938c4f0791e7077c250111bb0f9
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-04-13 16:36:18 +0200

    Refactore le package display/

---
+++ pfmg/lexique/display/Display.py
@@ -0,0 +1,49 @@
+"""Interface pour la représentation des objets."""
+from abc import ABC, abstractmethod
+
+from frozendict import frozendict
+
+from pfmg.lexique.stem_space.StemSpace import StemSpace
+
+
+class Display(ABC):
+    """Interface pour la représentation des objets."""
+
+    @abstractmethod
+    def to_string(self, term: StemSpace | str | None = None) -> str:
+        """Convertit un objet en string.
+
+        :param term:
+        :return:
+        """
+
+    @abstractmethod
+    def get_sigma(self) -> frozendict:
+        """Renvoie les propriété d'un objet.
+
+        :return:
+        """
+
+    @abstractmethod
+    def _to_string__stemspace(self, term: StemSpace) -> str:
+        """Inner function quand term est un stem_space.
+
+        :param term:
+        :return:
+        """
+
+    @abstractmethod
+    def _to_string__str(self, term: str) -> str:
+        """Inner function quand term est un string.
+
+        :param term:
+        :return:
+        """
+
+    @abstractmethod
+    def _to_string__nonetype(self, term: None = None) -> str:
+        """Inner function quand term est None.
+
+        :param term:
+        :return:
+        """
---
+++ pfmg/lexique/display/MixinDisplay.py
@@ -0,0 +1,29 @@
+"""Mixin pour la représentation des objects."""
+from pfmg.lexique.display.Display import Display
+from pfmg.lexique.stem_space.StemSpace import StemSpace
+
+
+class MixinDisplay(Display):
+    """Mixin qui implémente la factory to_string."""
+
+    def to_string(self, term: StemSpace | str | None = None) -> str:
+        """Transforme un objet en un string le décrivant.
+
+        :param term: radical utiliser pour représenter un objet
+            StemSpace: objet avec plusieurs radicaux
+            str: objet avec un seul radical
+            None: objet sans radical
+        :return: une string décrivant l'objet
+        """
+        return getattr(self, f"_to_string__{term.__class__.__name__.lower()}")(
+            term=term,
+        )
+
+    def _to_string__str(self, term: str) -> str:
+        raise NotImplementedError
+
+    def _to_string__nonetype(self, term: None = None) -> str:
+        raise NotImplementedError
+
+    def _to_string__stemspace(self, term: StemSpace) -> str:
+        raise NotImplementedError
