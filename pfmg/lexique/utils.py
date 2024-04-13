2b34b898fe0602bdb48ebd240cdd38bf811e4748
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-04-13 17:02:22 +0200

    Ajoute un utilitaire au package lexique/

---
+++ pfmg/lexique/utils.py
@@ -0,0 +1,22 @@
+"""Différents utilitaires."""
+from ast import literal_eval
+
+from frozendict import frozendict
+
+
+def dictify(chars: str) -> frozendict:
+    """Transforme une chaine de caractères en un frozendict.
+    
+    :param chars: Une chaine de caractère prête
+                  à être parsée et convertie en frozendict.
+    :return: un frozendict
+    """
+    return frozendict(
+        ({}
+         if chars == ""
+         else literal_eval(
+            ('{"'
+             + chars.replace("=", '":"').replace(",", '","')
+             + '"}'),
+        )),
+    )
