5bdd7f7dac86fdd09b7796c28b91caf0023260f3
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-04-30 00:19:17 +0200

    Ajoute l'appel au parseur NLTK

--- pfmg/parsing/parser/Parser.py
+++ pfmg/parsing/parser/Parser.py
@@ -4,31 +4,44 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 """TODO : Write some doc."""
-
 from collections.abc import Iterator
 from dataclasses import dataclass
+
+import nltk.grammar
+from nltk import EarleyChartParser
 
 from pfmg.lexique.lexicon import Lexicon
 from pfmg.lexique.sentence.Sentence import Sentence
 from pfmg.parsing.grammar import Grammar
-from pfmg.parsing.parsable import ABCParsable
+from pfmg.parsing.parsable.MixinParseParsable import MixinParseParsable
 
 
 @dataclass
-class Parser(ABCParsable):
+class Parser(MixinParseParsable):
     """TODO : Write some doc."""
 
     lexique: Lexicon
     grammar: Grammar
+    how: str
 
-    def parse_one(self, sent: str) -> Sentence | None:
+    def __post_init__(self):
         """TODO : Write some doc."""
-        raise NotImplementedError
+        grammar = nltk.grammar.FeatureGrammar.fromstring(
+            "\n\n".join(
+                (self.grammar.to_nltk(),
+                 getattr(self.lexique, f"to_{self.how}")())
+            )
+        )
+        self.parser = EarleyChartParser(grammar)
 
-    def parse_all(self, sent: str) -> Iterator[Sentence]:
-        """TODO : Write some doc."""
-        raise NotImplementedError
+    def _parse_first_str(self, data: str) -> Sentence:
+        pass
 
-    def parse_sents(self, sents: Iterator[str]) -> Iterator[Iterator[Sentence]]:
-        """TODO : Write some doc."""
-        raise NotImplementedError
+    def _parse_first_list(self, data: list[str]) -> Iterator[Sentence]:
+        pass
+
+    def _parse_all_str(self, data: str) -> Iterator[Sentence]:
+        pass
+
+    def _parse_all_list(self, data: list[str]) -> Iterator[Sentence]:
+        pass
