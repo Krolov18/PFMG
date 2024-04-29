dfcd2fe62667b9be4536f71ac95eb26b6faeb663
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-04-29 22:25:00 +0200

    Retire inhéritance inutile

--- pfmg/parsing/grammar/Grammar.py
+++ pfmg/parsing/grammar/Grammar.py
@@ -7,21 +7,18 @@
 
 from dataclasses import dataclass
 
-from frozendict import frozendict
-
-from pfmg.external.display import ABCDisplay
 from pfmg.lexique.stem_space.StemSpace import StemSpace
 from pfmg.parsing.production import Production
 
 
 @dataclass
-class Grammar(ABCDisplay):
+class Grammar:
     """TODO : Write some doc."""
 
     start: str
     productions: list[Production]
 
-    def to_string(self, term: StemSpace | str | None = None) -> str:
+    def to_nltk(self, term: StemSpace | str | None = None) -> str:
         """Convertit la Grammar en une chaîne de caractère pour NLTK.
 
         :param term: inutile pour cette implémentation
@@ -30,22 +27,6 @@ class Grammar(ABCDisplay):
         return "\n\n".join(
             (
                 f"% start {self.start}",
-                "\n".join(x.to_string() for x in self.productions),
+                "\n".join(x.to_nltk() for x in self.productions),
             )
         )
-
-    def get_sigma(self) -> frozendict:
-        """TODO : Write some doc."""
-        raise NotImplementedError
-
-    def _to_string__stemspace(self, term: StemSpace) -> str:
-        """TODO : Write some doc."""
-        raise NotImplementedError
-
-    def _to_string__str(self, term: str) -> str:
-        """TODO : Write some doc."""
-        raise NotImplementedError
-
-    def _to_string__nonetype(self, term: None = None) -> str:
-        """TODO : Write some doc."""
-        raise NotImplementedError
