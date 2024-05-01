8d8b9132136c31276251fd373713a78aad73881f
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-05-01 23:27:34 +0200

    Met à jour les copyrights

--- pfmg/test/lexique/glose/test_gloses.py
+++ pfmg/test/lexique/glose/test_gloses.py
@@ -3,11 +3,6 @@
 
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
- # Copyright (c) 2024, Korantin Lévêque <korantin.leveque@protonmail.com>
-# All rights reserved.
-
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 import pytest
 import yaml
 from frozendict import frozendict
