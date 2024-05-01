cf3e05f751e8ade9dd1f1f11b5477223807d2e59
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-05-01 23:19:37 +0200

    Met à jour les copyrights

--- pfmg/parsing/tokenizer/SpaceTokenizer.py
+++ pfmg/parsing/tokenizer/SpaceTokenizer.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024, Korantin Lévêque <korantin.leveque@protonmail.com>
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 """Tokenizer sur les espaces."""
 
 from pfmg.parsing.tokenizer.TokenizerReMixin import TokenizerReMixin
