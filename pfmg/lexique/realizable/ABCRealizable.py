5621907a39a4013b046ad9b85c5856cb6b7574fb
Krolov18 <Krolov18@users.noreply.github.com>
2024-05-11 00:44:34 +0200

    72 corrige les copyrights (#73)
    * #72 : Ajoute les copyright à Desinence.py
    
    * #72 : Retire la ligne 3 de tous les fichiers *.py

--- pfmg/lexique/realizable/ABCRealizable.py
+++ pfmg/lexique/realizable/ABCRealizable.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2024, Korantin Lévêque <korantin.leveque@protonmail.com>
 # All rights reserved.
-
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 """ABCRealizer."""
