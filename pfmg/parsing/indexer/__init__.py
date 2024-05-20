1bb3a0b66be2d42dad20b36afccadab097de985d
Korantin Lévêque <korantin.leveque@protonmail.com>
2024-05-20 06:52:05 +0200

    chore: ajoute les copyrights aux nouveaux fichiers

--- pfmg/parsing/indexer/__init__.py
+++ pfmg/parsing/indexer/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (c) 2024, Korantin Lévêque <korantin.leveque@protonmail.com>
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 from pfmg.parsing.indexer.ABCIndexer import ABCindexer, new_indexer
